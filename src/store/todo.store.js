d16b2060f5b340741a247b171d94dea95a48803f
MaYdaN875 <danielleon0100@gmail.com>
2024-10-11 19:25:40 -0600

    Added all backend code

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,6 +1,6 @@
 import {Todo} from '../todos/models/todo.model';
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
@@ -10,17 +10,98 @@ const state = {
     todos: [
         new Todo('Js'),
         new Todo('Node'),
-        new Todo('Js'),
+        new Todo('React'),
+        new Todo('Python'),
+        
+         
     ],
     filter: Filters.All,
 }
 
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('InitStore 🐱');
 }
 
+const loadStore = ()=>{
+    if(!localStorage.getItem('state'))return;
+
+    const {todos = [],filter = Filters.All} = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos;
+    state.filter = filter;
+}
+
+const saveStateToLocalStorage =() =>{
+    localStorage.setItem('state',JSON.stringify(state));
+
+    
+}
+
+const getTodos = ( filter = Filters.All) =>{
+    switch (filter){
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        
+        default:
+            throw new Error (`Option ${filter} is not valid.`)
+        
+    }
+}
+
+const addTodo = (description)=>{
+    if (!description)  throw new Error('Description Is required');
+
+    state.todos.push(new Todo(description));
+
+    saveStateToLocalStorage();
+}
+
+const toggleTodo = (todoId) =>{
+
+   state.todos = state.todos.map(todo=>{
+        if(todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+   });
+   saveStateToLocalStorage();
+}
+
+const deleteTodo = (todoId)=>{
+    state.todos = state.todos.filter( todo=> todo.id !== todoId);
+
+    saveStateToLocalStorage();
+    
+}
+const deleteTodoCompleted = ()=>{
+    state.todos = state.todos.filter( todo=> !todo.done);
+    
+    saveStateToLocalStorage();
+}
+
+const setFilter = (newFilter = Filters.All)=>{
+    state.filter = newFilter;
+    saveStateToLocalStorage();
+}
+
+const getCurrentFilter = () =>{
+    return state.filter;
+}
 export default {
+    addTodo,
+    deleteTodo,
+    deleteTodoCompleted,
+    getCurrentFilter,
     initStore,
+    getTodos,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,15 +1,95 @@
-
 import html from './app.html?raw';
+import todoStore, { Filters } from '../store/todo.store'
+import { renderTodos, renderPending } from './use-cases';
 
+const elementIDs = {
+    ClearCompleted: '.clear-completed',
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+    TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
+} 
+export const App = (elementId)=>{
 
-export const App=(elementId)=>{
+    const displayTodos = ()=>{
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        console.log(todos);
+        renderTodos( elementIDs.TodoList, todos);
+        updatePendingCount();
+    }
 
+    const updatePendingCount = ()=>{
+        renderPending(elementIDs.PendingCountLabel);
+    }
 
     (()=>{
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
 
     })();
 
+
+    //Referencias HTML
+    const newDescriptionInput = document.querySelector(elementIDs.NewTodoInput);
+    const todoListUL = document.querySelector( elementIDs.TodoList);
+    const ClearCompletedButton = document.querySelector(elementIDs.ClearCompleted);
+    const filtersLIs = document.querySelectorAll( elementIDs.TodoFilters);
+
+    newDescriptionInput.addEventListener('keyup', (event) =>{
+        if(event.keyCode !== 13) return;
+        if(event.target.value.trim().length === 0 )return;
+
+        todoStore.addTodo( event.target.value);
+        displayTodos();
+        event.target.value = '';
+
+    });
+
+    todoListUL.addEventListener('click',(event)=>{
+       const element = event.target.closest('[data-id]');
+       todoStore.toggleTodo(element.getAttribute('data-id'));
+       displayTodos();
+    });
+
+    todoListUL.addEventListener('click',(event)=>{
+        if (event.target.className !== 'destroy')return;
+
+        const element = event.target.closest('[data-id]');
+        todoStore.deleteTodo(element.getAttribute('data-id'));
+        displayTodos();
+
+     });
+
+     ClearCompletedButton.addEventListener('click',()=>{
+        todoStore.deleteTodoCompleted();
+        displayTodos();
+     });
+
+     filtersLIs.forEach( element =>{
+
+        element.addEventListener('click', (element) =>{
+            filtersLIs.forEach(el => el.classList.remove('selected'));
+            element.target.classList.add('selected');
+
+            
+
+            switch(element.target.text){
+                case 'Todos':
+                    todoStore.setFilter(Filters.All);
+                break;
+                case 'Completados':
+                    todoStore.setFilter(Filters.Completed);
+                break;
+                case 'Pendientes':
+                    todoStore.setFilter(Filters.Pending);
+                break;
+            }
+
+            displayTodos();
+
+        });
+
+     });
 }
